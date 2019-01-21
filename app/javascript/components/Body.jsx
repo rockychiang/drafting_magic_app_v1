c038dc869da8de1edfd2c4f4dc051dcfddcb20eb
rockychiang <rocky@chem.ucla.edu>
2019-01-21 05:17:56 -0500

    initialize body component state

--- app/javascript/components/Body.jsx
+++ app/javascript/components/Body.jsx
@@ -1,10 +1,27 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Format from "./Format.jsx"
+import Block from "./Block.jsx"
 
 class Body extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      ongoing: false,
+      block: "grn",
+      format: "draft"
+    }
+  }
+
   render () {
     return (
       <React.Fragment>
+        <form onSubmit={this.handleSubmit}>
+          <Block />
+          <Format />
+          <input type="submit" value="Start" />
+        </form>
       </React.Fragment>
     );
   }
