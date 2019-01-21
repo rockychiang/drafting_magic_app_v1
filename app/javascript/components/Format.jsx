a8c07ce8d040021a86c71176da7ccf22452afc24
rockychiang <rocky@chem.ucla.edu>
2019-01-21 05:18:18 -0500

    create block and format component

---
+++ app/javascript/components/Format.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Format extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Format
