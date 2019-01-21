91fb892779dfee7f4943dd6a64efd11c51384e77
rockychiang <rocky@chem.ucla.edu>
2019-01-21 05:24:57 -0500

    create dropdown menu for Block component

--- app/javascript/components/Block.jsx
+++ app/javascript/components/Block.jsx
@@ -1,12 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
-class Set extends React.Component {
+
+class Block extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <label>
+          Set:
+          <select id="set" name="set" onChange={this.props.onChange}>
+            <option value="rna" disabled>Ravnica Allegiance (coming soon)</option>
+            <option value="grn" selected>Guilds of Ravnica</option>
+            <option value="dom" disabled>Dominaria (coming soon)</option>
+          </select>
+        </label>
       </React.Fragment>
     );
   }
 }
 
-export default Set
+export default Block
