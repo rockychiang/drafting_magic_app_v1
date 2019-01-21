42950495de89087831b053d92680a622e647e423
rockychiang <rocky@chem.ucla.edu>
2019-01-21 06:15:53 -0500

    configure draft controller to respond with the correct data

--- app/controllers/api/v1/drafts_controller.rb
+++ app/controllers/api/v1/drafts_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::DraftsController < Api::V1::BaseController
+  include DraftsHelper
 
   def create
+    packs = generate_packs(draft_params[:block], draft_params[:format])
+    respond_with packs, json: packs
   end
 
   private
