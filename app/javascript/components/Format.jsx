30017f03b6080b2aa01ea36fde97897cdd0c3e47
rockychiang <rocky@chem.ucla.edu>
2019-01-21 05:25:42 -0500

    create radio options for Format component

--- app/javascript/components/Format.jsx
+++ app/javascript/components/Format.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+
 class Format extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <label>
+          Format:
+          <input type="radio" name="format" value="draft" onChange={this.props.onChecked} defaultChecked />Draft
+          <input type="radio" name="format" value="sealed" onChange={this.props.onChecked} />Sealed
+        </label>
       </React.Fragment>
     );
   }
