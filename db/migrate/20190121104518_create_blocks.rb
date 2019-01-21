b291428b2ffbfdff78d214c9ad68f468b4f56083
rockychiang <rocky@chem.ucla.edu>
2019-01-21 05:58:07 -0500

    create migration and schema files

--- db/migrate/20190121104518_create_blocks.rb
+++ db/migrate/20190121104518_create_blocks.rb
@@ -1,8 +1,11 @@
 class CreateBlocks < ActiveRecord::Migration[5.2]
   def change
     create_table :blocks do |t|
+      t.string :name
+      t.string :code
 
       t.timestamps
     end
+    add_index :blocks, :code, unique: true
   end
 end
--- db/migrate/20190121104532_create_cards.rb
+++ db/migrate/20190121104532_create_cards.rb
@@ -1,6 +1,15 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
+      t.string :name
+      t.integer :block_id
+      t.string :rarity
+      t.integer :cmc
+      t.text :colors, array: true
+      t.text :types, array: true
+      t.decimal :rating, precision: 2, scale: 1
+      t.string :imgurl
+      t.boolean :rare_draft, default: 0
 
       t.timestamps
     end
