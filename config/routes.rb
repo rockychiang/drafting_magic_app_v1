2b86d8d9ac4d9ece1eb84722126166320251f20e
rockychiang <rocky@chem.ucla.edu>
2019-01-21 06:10:56 -0500

    configure API route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'site#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :drafts, only: :create, defaults: { format: JSON }
+    end
+  end
 end
