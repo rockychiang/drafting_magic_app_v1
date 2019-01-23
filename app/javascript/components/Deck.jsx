35337e62bf38b7c75f5596cee3106025effce41f
rockychiang <rocky@chem.ucla.edu>
2019-01-23 18:03:21 -0500

    divide cards into their cmc for deck components

--- app/javascript/components/Deck.jsx
+++ app/javascript/components/Deck.jsx
@@ -4,16 +4,36 @@ import Stack from "./Stack.jsx"
 
 class Deck extends React.Component {
   render () {
-    let body
+    let onebody
+    let twobody
+    let threebody
+    let fourbody
+    let fivebody
+    let sixbody
     if (this.props.cards) {
-      let one = this.props.cards.filter(card => card.cmc < 4)
-      body = <Stack cards={one} />
+      let one = this.props.cards.filter(card => card.cmc < 2)
+      let two = this.props.cards.filter(card => card.cmc === 2)
+      let three = this.props.cards.filter(card => card.cmc === 3)
+      let four = this.props.cards.filter(card => card.cmc === 4)
+      let five = this.props.cards.filter(card => card.cmc === 5)
+      let six = this.props.cards.filter(card => card.cmc > 5)
+      onebody = <Stack cards={one} />
+      twobody = <Stack cards={two}  />
+      threebody = <Stack cards={three} />
+      fourbody = <Stack cards={four} />
+      fivebody = <Stack cards={five} />
+      sixbody = <Stack cards={six} />
     }
 
     return (
       <React.Fragment>
         <div id="deck">
-          {body}
+          {onebody}
+          {twobody}
+          {threebody}
+          {fourbody}
+          {fivebody}
+          {sixbody}
         </div>
       </React.Fragment>
     );
--- app/javascript/components/Stack.jsx
+++ app/javascript/components/Stack.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 class Stack extends React.Component {
   render () {
     let stack = this.props.cards.map((card, i) => {
-      let style = { top: i*27 + 'px' }
+      let style = { top: 20+i*27 + 'px' }
 
       return (
         <img className="card deck" src={card.imgurl} alt={card.name} key={i} style={style} />
@@ -13,7 +13,10 @@ class Stack extends React.Component {
 
     return (
       <React.Fragment>
-        {stack}
+        <div className="stacks">
+          <p className="count">{this.props.cards.length}</p><br></br>
+          {stack}
+        </div>
       </React.Fragment>
     );
   }
