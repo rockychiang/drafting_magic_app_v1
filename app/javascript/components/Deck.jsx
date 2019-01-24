18ff4b939a6352a2b1315ae863486ecaa47be5f2
rockychiang <rocky@chem.ucla.edu>
2019-01-24 10:03:46 -0500

    add sideboard ui

--- app/javascript/components/Deck.jsx
+++ app/javascript/components/Deck.jsx
@@ -15,11 +15,8 @@ class Deck extends React.Component {
     let creatures
     let total
     let others
+
     if (this.props.cards) {
-      lands = this.props.cards.filter(card => card.types.includes("Land")).length
-      creatures = this.props.cards.filter(card => card.types.includes("Creature")).length
-      total = this.props.cards.length
-      others = total - lands - creatures
       let one = this.props.cards.filter(card => card.cmc < 2 && !card.types.includes("Land"))
       let two = this.props.cards.filter(card => card.cmc === 2)
       let three = this.props.cards.filter(card => card.cmc === 3)
@@ -34,15 +31,19 @@ class Deck extends React.Component {
       fivebody = <Stack cards={five} />
       sixbody = <Stack cards={six} />
       landbody = <Stack cards={land} />
+      lands = this.props.cards.filter(card => card.types.includes("Land")).length
+      creatures = this.props.cards.filter(card => card.types.includes("Creature")).length
+      total = this.props.cards.length
+      others = total - lands - creatures
     }
 
     return (
       <React.Fragment>
         <div id="deck-pool">
-          <div id="info-bar">
-            <text>Lands: {lands}    Creatures: {creatures}    Others: {others}    Total: {total}</text>
+          <div className="info-bar">
+            <span>Lands: {lands}    Creatures: {creatures}    Others: {others}    Total: {total}</span>
           </div>
-          <div id="stacks">
+          <div id="deck-stacks">
             {onebody}
             {twobody}
             {threebody}
@@ -50,6 +51,15 @@ class Deck extends React.Component {
             {fivebody}
             {sixbody}
             {landbody}
+          </div>
+        </div>
+
+        <div id="side-pool">
+          <div className="info-bar">
+            <span>Sideboard: {total}</span>
+          </div>
+          <div id="pool-stacks">
+            {onebody}
           </div>
         </div>
       </React.Fragment>
