7473d07b2c7168d50e54fdfb23ba0d2e4ccd8247
rockychiang <rocky@chem.ucla.edu>
2019-01-25 21:54:08 -0500

    create sortBy function and its first category: name

---
+++ app/javascript/utils/sortBy.js
@@ -0,0 +1,26 @@
+export default function sortBy(category, cards) {
+  let sortedCards
+  switch (category) {
+    case "name":
+      sortedCards = cards.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+      break;
+    case "color":
+      sortedCards = cards.sort((a, b) => {
+        
+      })
+      break;
+    case "cmc":
+      sortedCards
+      break;
+    case "rarity":
+      sortedCards
+      break;
+    default:
+      sortedCards = cards.sort();
+  }
+  return sortedCards;
+}
