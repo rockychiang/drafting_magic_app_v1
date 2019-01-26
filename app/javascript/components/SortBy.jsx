fbf20ba2685291eebde232e4e1e2915f95588706
rockychiang <rocky@chem.ucla.edu>
2019-01-25 19:47:32 -0500

    create SortBy component

---
+++ app/javascript/components/SortBy.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class SortBy extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <select className="sort" name="sort" onChange={this.props.onChange} defaultValue="color">
+          <option value="color">Color</option>
+          <option value="cmc">Converted Mana Cost</option>
+          <option value="rarity">Rarity</option>
+        </select>
+      </React.Fragment>
+    );
+  }
+}
+
+export default SortBy
--- app/javascript/components/Stack.jsx
+++ app/javascript/components/Stack.jsx
@@ -5,7 +5,7 @@ class Stack extends React.Component {
   render () {
     let stack = this.props.cards.sort((a, b) => {
       if (a.name < b.name) return -1;
-      else if (a.name > b.name) return 1;
+      if (a.name > b.name) return 1;
       return 0;
     }).map((card, i) => {
       let style = { top: 20+i*27 + 'px' }
