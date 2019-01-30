a2b3bf1acf5404e4a990042062b32ff7fe0553c9
rockychiang <rocky@chem.ucla.edu>
2019-01-30 18:02:43 -0500

    rename SideBar component to DeckList

---
+++ app/javascript/components/DeckList.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class DeckList extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div>
+          <div>
+          </div>
+        </div>
+        <div>
+          <div>
+          </div>
+        </div>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default DeckList
