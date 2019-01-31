67c4b8c6b477904e5cf54b800bda915814bc3a08
rockychiang <rocky@chem.ucla.edu>
2019-01-30 21:08:22 -0500

    make DeckList component list the card by name

--- app/javascript/components/DeckList.jsx
+++ app/javascript/components/DeckList.jsx
@@ -1,16 +1,32 @@
 import React from "react"
 import PropTypes from "prop-types"
+import sortBy from "../utils/sortBy.js"
 
 class DeckList extends React.Component {
   render () {
     let { cards, handleClick, handleHover } = this.props;
     let total = cards.length;
+    let cardlist = sortBy("cmc", cards).map((card, i) => {
+
+      return (
+        <img
+          key={i}
+          className="card list"
+          alt={card.name}
+          src={card.imgurl}
+          onClick={handleClick}
+          onMouseOver={handleHover}
+        />
+      )
+    })
+
     return (
       <div id="deck-list">
         <div className="info-bar">
           <span className="info-bar-text">Total: {total}</span>
         </div>
         <div id="card-list">
+          {cardlist}
         </div>
       </div>
     );
