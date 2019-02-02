46cdb2a4144024dea1c1a3dc42f05d18a3662300
rockychiang <rocky@chem.ucla.edu>
2019-02-02 18:12:35 -0500

    refactor botPick function to use takeCardBy function

--- app/javascript/utils/botPick.js
+++ app/javascript/utils/botPick.js
@@ -1,7 +1,7 @@
+import takeCardBy from "./takeCardBy.js"
+
 export default function botPick(pack, pickNo) {
   let packCopy = pack.slice(0);
   let maxRating = Math.max.apply(Math, packCopy.map(card => card.rating));
-  let cardIndex = packCopy.findIndex(card => card.rating == maxRating);
-  let card = pack.splice(cardIndex, 1);
-  return card;
+  return takeCardBy("rating", maxRating, pack);
 }
