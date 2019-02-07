fecb1c168e8b2c0287f134660c46067357e1a155
rockychiang <rocky@chem.ucla.edu>
2019-02-07 15:17:57 -0500

    refactor adjustRatingBy to its own function

--- app/javascript/utils/RatingAdjusters/rna.js
+++ app/javascript/utils/RatingAdjusters/rna.js
@@ -1,26 +1,46 @@
-export default function adjustRatingForRNA(bot, pack , block) {
+import adjustRatingBy from "./adjustRatingBy.js"
+
+export default function adjustRatingForRNA(bot, pack, block) {
   const pickNo = bot.length + 1;
   if (pickNo === 2) {
     const fpColor = bot[0].colors
-    console.log(bot[0])
-    if (fpColor.length === 1) {
-      pack.filter(card => card.colors.includes(fpColor[0])).map(card => card.rating = card.rating + 0.2)
-    } else {
-      let filtered = pack.filter(card => {
-        return (
-          ( card.colors.length == 1 && card.colors.includes(fpColor[0]) ) ||
-          ( card.colors.length == 1 && card.colors.includes(fpColor[1]) ) ||
-          ( card.colors.includes(fpColor[0]) && card.colors.includes(fpColor[1]) )
-        )
-      })
-      filtered.map(card => card.rating = card.rating + 0.2)
-    }
+    adjustRatingBy(0.2, fpColor, pack);
   } else if (pickNo > 2 && pickNo < 5) {
-
+    const colors = bot.map(card => card.colors).flat();
+    const uniqColors = [...new Set(colors)];
+    // let filtered = pack.filter(card => {
+    //   return (
+    //
+    //   )
+    // })
   } else {
 
   }
 }
+
+// 2 different guilds that share no color 4 4
+//   pick card that supports either guilds
+// 1 guild and 1 mono-color that doesn't share color 3 3
+//   pick card that supports either cards
+// 2 different guilds that share a color 4 3
+//   pick card that includes that color
+// 2 mono-color of different colors that doesn't support a guild 2 2
+//   pick card that supports either color
+// 1 guild and 1 mono-color or 2 cards of the same guild 2/3/4 2
+//   pick card that supports that guild
+// 2 mono-color of the same color 2 1
+//   pick card that supports that color
+
+// 2 different guilds that share no color 5/6 4
+//   pick card that supports either guilds
+// 1 guild and 1 mono-color that doesn't share color 4/5 3 or 5 4
+//   pick card that supports either cards
+// 2 different guilds that share a color 5/6 3
+//   pick card that includes that color
+// 1 guild and 1 mono-color or 2 cards of the same guild 4/5/6 2
+//   pick card that supports that guild
+// 3 mono-color 3 1
+//   pick card that supports that color
 
   // switch(fpColor[0]) {
   //   case "W":
