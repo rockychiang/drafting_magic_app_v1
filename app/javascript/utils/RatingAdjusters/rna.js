9ce842fefc1e44ed2e6cc68861b153a00928d230
rockychiang <rocky@chem.ucla.edu>
2019-02-06 21:04:06 -0500

    create rating adjusters file for grn and rna

---
+++ app/javascript/utils/RatingAdjusters/rna.js
@@ -0,0 +1,10 @@
+export default function adjustRatingForGRN(bot, pack , block) {
+  const pickNo = bot.length + 1;
+  if (pickNo < 4) {
+
+  } else if (pickNo > 4 && pickNo < 7) {
+
+  } else {
+
+  }
+}
