f1a030bf2f27d508a3ee7e4c2f7b66cfccb5c990
rockychiang <rocky@chem.ucla.edu>
2019-02-07 15:18:32 -0500

    create adjustRatingBy function

---
+++ app/javascript/utils/RatingAdjusters/adjustRatingBy.js
@@ -0,0 +1,40 @@
+export default function adjustRatingBy(adjustment, color, pack) {
+  let filtered;
+  switch (color.length) {
+    case 1:
+      filtered = pack.filter(card => {
+        return (
+          card.colors.includes(color[0])
+        )
+      });
+      break;
+
+    case 2:
+      filtered = pack.filter(card => {
+        return (
+          (card.colors.length == 1 && card.colors.includes(color[0])) ||
+          (card.colors.length == 1 && card.colors.includes(color[1])) ||
+          (card.colors.includes(color[0]) && card.colors.includes(color[1]))
+        )
+      });
+      break;
+
+    case 3:
+      filtered = pack.filter(card. => {
+        return (
+          (card.colors.length == 1 && card.colors.includes(color[0])) ||
+          (card.colors.length == 1 && card.colors.includes(color[1])) ||
+          (card.colors.length == 1 && card.colors.includes(color[2])) ||
+          (card.colors.length == 2 && (card.colors.includes(color[0]) && card.colors.includes(color[1]))) ||
+          (card.colors.length == 2 && (card.colors.includes(color[0]) && card.colors.includes(color[2]))) ||
+          (card.colors.length == 2 && (card.colors.includes(color[1]) && card.colors.includes(color[2]))) ||
+          (card.colors.includes(color[0]) && card.colors.includes(color[1]) && card.colors.includes(color[2]))
+        )
+      });
+      break;
+
+    default:
+
+  }
+  filtered.map(card => card.rating = card.rating + adjustment);
+}
