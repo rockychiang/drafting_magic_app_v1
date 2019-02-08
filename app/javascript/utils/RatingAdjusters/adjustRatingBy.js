8b5162dfb295a8e188216ebbf6c99aeb5a21f3e4
rockychiang <rocky@chem.ucla.edu>
2019-02-07 23:17:18 -0500

    refactor filterByColor into its own file

--- app/javascript/utils/RatingAdjusters/adjustRatingBy.js
+++ app/javascript/utils/RatingAdjusters/adjustRatingBy.js
@@ -1,40 +1,6 @@
+import filterByColor from "./filterByColor.js"
+
 export default function adjustRatingBy(adjustment, color, pack) {
-  let filtered;
-  switch (color.length) {
-    case 1:
-      filtered = pack.filter(card => {
-        return (
-          card.colors.includes(color[0])
-        )
-      });
-      break;
-
-    case 2:
-      filtered = pack.filter(card => {
-        return (
-          (card.colors.length == 1 && card.colors.includes(color[0])) ||
-          (card.colors.length == 1 && card.colors.includes(color[1])) ||
-          (card.colors.includes(color[0]) && card.colors.includes(color[1]))
-        )
-      });
-      break;
-
-    case 3:
-      filtered = pack.filter(card => {
-        return (
-          (card.colors.length == 1 && card.colors.includes(color[0])) ||
-          (card.colors.length == 1 && card.colors.includes(color[1])) ||
-          (card.colors.length == 1 && card.colors.includes(color[2])) ||
-          (card.colors.length == 2 && (card.colors.includes(color[0]) && card.colors.includes(color[1]))) ||
-          (card.colors.length == 2 && (card.colors.includes(color[0]) && card.colors.includes(color[2]))) ||
-          (card.colors.length == 2 && (card.colors.includes(color[1]) && card.colors.includes(color[2]))) ||
-          (card.colors.includes(color[0]) && card.colors.includes(color[1]) && card.colors.includes(color[2]))
-        )
-      });
-      break;
-
-    default:
-
-  }
-  filtered.map(card => card.rating = card.rating + adjustment);
+  let filteredPack = filterByColor(color, pack);
+  filteredPack.map(card => card.rating = card.rating + adjustment);
 }
