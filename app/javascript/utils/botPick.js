79068258014f86da5cc46a0f8f73c093adc2077f
rockychiang <rocky@chem.ucla.edu>
2019-02-07 23:13:46 -0500

    change how cards are picked in botPick

--- app/javascript/utils/botPick.js
+++ app/javascript/utils/botPick.js
@@ -3,16 +3,15 @@ import adjustRatingForGRN from "./RatingAdjusters/grn.js"
 import adjustRatingForRNA from "./RatingAdjusters/rna.js"
 
 export default function botPick(bot, pack, block) {
+  const pickNo = bot.length + 1;
   let packCopy = JSON.parse(JSON.stringify(pack));
-  adjustRating(bot, packCopy, block);
-  const maxRating = Math.max.apply(Math, packCopy.map(card => card.rating));
-  const card = packCopy.filter(card => card.rating == maxRating)[0];
+  adjustRating(bot, packCopy, block, pickNo);
+  const card = pickBestCard(packCopy, bot, pickNo)
   console.log(bot, pack, card)
   return takeCardBy("name", card.name, pack);
 }
 
-function adjustRating(bot, pack, block) {
-  const pickNo = bot.length + 1;
+function adjustRating(bot, pack, block, pickNo) {
   if (pickNo === 1) {
     pack.filter(card => card.colors.length == 1).map(card => card.rating = card.rating + 0.1)
   } else {
@@ -24,6 +23,17 @@ function adjustRating(bot, pack, block) {
         adjustRatingForRNA(bot, pack, block);
         break;
       default:
+    }
+  }
+}
+
+function pickBestCard(pack, bot, pickNo) {
+  pack.sort(card1, card2 => card1.rating - card2.rating);
+  if (pickNo === 1) {
+    return pack[0]
+  } else if (pickNo === 2) {
+    if (bot[0].rating >= 4) {
+
     }
   }
 }
