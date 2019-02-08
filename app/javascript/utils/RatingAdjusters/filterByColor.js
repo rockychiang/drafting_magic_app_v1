71aa3046157eb75147ebbc495058025a95d92e99
rockychiang <rocky@chem.ucla.edu>
2019-02-07 23:14:09 -0500

    create filterByColor function

---
+++ app/javascript/utils/RatingAdjusters/filterByColor.js
@@ -0,0 +1,40 @@
+export default function filterByColor(colors, pack) {
+  let filteredPack
+  switch (colors.length) {
+    case 1:
+      filteredPack = pack.filter(card => {
+        return (
+          card.colors.includes(color[0])
+        )
+      });
+      break;
+
+    case 2:
+      filteredPack = pack.filter(card => {
+        return (
+          (card.colors.length == 1 && card.colors.includes(color[0])) ||
+          (card.colors.length == 1 && card.colors.includes(color[1])) ||
+          (card.colors.includes(color[0]) && card.colors.includes(color[1]))
+        )
+      });
+      break;
+
+    case 3:
+      filteredPack = pack.filter(card => {
+        return (
+          (card.colors.length == 1 && card.colors.includes(color[0])) ||
+          (card.colors.length == 1 && card.colors.includes(color[1])) ||
+          (card.colors.length == 1 && card.colors.includes(color[2])) ||
+          (card.colors.length == 2 && (card.colors.includes(color[0]) && card.colors.includes(color[1]))) ||
+          (card.colors.length == 2 && (card.colors.includes(color[0]) && card.colors.includes(color[2]))) ||
+          (card.colors.length == 2 && (card.colors.includes(color[1]) && card.colors.includes(color[2]))) ||
+          (card.colors.includes(color[0]) && card.colors.includes(color[1]) && card.colors.includes(color[2]))
+        )
+      });
+      break;
+
+    default:
+      filteredPack = pack;
+  }
+  return filteredPack;
+}
